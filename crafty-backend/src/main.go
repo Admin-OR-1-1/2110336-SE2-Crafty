8b28f8e2760f52b05d9b6e1450b40675532becad
Kuranasaki@M3Max <ironpan10@hotmail.com>
2024-01-16 12:17:54 +0700

    Revert "Make world nigga only"
    This reverts commit 1d74affe43eddfcd27f3210371e89705e5c8ec7e.

--- crafty-backend/src/main.go
+++ crafty-backend/src/main.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println("Hello, World nigga!")
-  fmt.Println("End, World nigga!")
+	fmt.Println("Hello, World nigga!")
+	fmt.Println("End, World nigga!")
+	fmt.Println("Hello, World!")
 }
