4ff33d9ce56e41e365bf7a850b40ff0d7b13da62
TitorPs360 <TitorPs360Gamer@gmail.com>
2024-01-16 10:40:04 +0700

    [feat] Add daisy ui

---
+++ crafty-frontend/src/app/_components/ui/button.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+interface ButtonProps {
+  onClick: () => void;
+}
+
+const Button: FC<ButtonProps> = ({ onClick }) => {
+  return <></>;
+};
+
+export default Button;
