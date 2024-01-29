9c76129ec666ce60b29e6c9e9ae12343a2c362e7
Kuranasaki@M3Max <ironpan10@hotmail.com>
2024-01-29 14:24:50 +0700

    WIP

---
+++ crafty-backend/internal/pkg/post/post.go
@@ -0,0 +1,34 @@
+package post
+
+type TThumbnail struct{
+	ThumbnailUrl string
+	ThumbnailType string
+}
+
+type TReview struct{
+	RatingStar float32
+	Comment string
+	UID string
+}
+
+type TPackage struct{
+	Price float64
+}
+
+type TPost struct {
+	ID string
+	Thumbnail TThumbnail
+	Name string
+	ReviewList []TReview
+	PackageList []TPackage
+	Detail string
+	Content string
+	OwnerID string
+}
+
+type IPost interface {
+	CreatePost(Post TPost) error
+	EditPost()
+	RemovePost()
+	ViewPost()
+}
