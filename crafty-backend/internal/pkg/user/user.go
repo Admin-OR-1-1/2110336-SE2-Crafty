45488434483b966223fd49dc49d118691cd254a8
KorawutMing <6432004421@student.chula.ac.th>
2024-01-29 13:07:02 +0700

    feat(User): :zap: define User struct
    Address struct and User struct

--- crafty-backend/internal/pkg/user/user.go
+++ crafty-backend/internal/pkg/user/user.go
@@ -3,13 +3,17 @@ package user
 import "google.golang.org/genproto/googleapis/type/phone_number"
 
 type Address struct {
-
+	Address_1   string
+	Street      string
+	Tambon      string
+	Amphoe      string
+	Province    string
+	Postal_code string
 }
 
 type User struct {
-  UID string
-  Username string
-  Phone_number phone_number.PhoneNumber
-  Address Address
-  
- }
+	UID          string
+	Username     string
+	Phone_number phone_number.PhoneNumber
+	Address      Address
+}
