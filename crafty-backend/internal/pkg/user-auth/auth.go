b9b2e6879df58216faeb853b5d4957711aa90dd2
Kuranasaki@M3Max <ironpan10@hotmail.com>
2024-01-29 14:42:12 +0700

    WIP

--- crafty-backend/internal/pkg/user-auth/auth.go
+++ crafty-backend/internal/pkg/user-auth/auth.go
@@ -1,5 +1,22 @@
 package auth
 
+import (
+	"github.com/Admin-OR-1-1/2110336-SE2-Crafty/crafty-backend/internal/pkg/infrastructure"
+	repo "github.com/Admin-OR-1-1/2110336-SE2-Crafty/crafty-backend/internal/pkg/user-auth/repo"
+)
+
 type Auth struct {
-  
+  Repo repo.AuthRepo
+}
+
+func NewAuth(i *infrastructure.Infrastructure) *Auth {
+  firebaseRepo := repo.CreateFirebaseAuthRepo(i)
+  return &Auth{
+    Repo: firebaseRepo,
+  }
+}
+
+func (a *Auth) VerifyToken(token string) (bool, error) {
+  verified, _, err := a.Repo.VerifyToken(token)
+  return verified, err
 }
