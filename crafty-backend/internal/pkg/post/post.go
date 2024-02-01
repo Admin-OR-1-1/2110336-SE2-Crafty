f36c000350d0ac0648046a0666a050eba39eba42
JinnySarita <6432181821@student.chula.ac.th>
2024-02-01 19:22:10 +0700

    [EDIT] delete post

--- crafty-backend/internal/pkg/post/post.go
+++ crafty-backend/internal/pkg/post/post.go
@@ -2,6 +2,7 @@ package post
 
 import (
 	postRepo "github.com/Admin-OR-1-1/2110336-SE2-Crafty/crafty-backend/internal/pkg/post/repo"
+	"errors"
 )
 
 /*
@@ -15,42 +16,34 @@ import (
 func CreatePost(Post postRepo.TPost) error {
 	return nil
 }
-func UpdatePost(Post postRepo.TPost) error {
-	return nil
-}
-func DeletePost(ID string) error {
-	return nil
-}
-func GetPostById(ID string) (postRepo.TPost, error) {
-	//for test only!!!!
-	if ID == "123456" {
-		ValidTThumbnail := postRepo.TThumbnail{
-			ThumbnailUrl:  "www.123.com",
-			ThumbnailType: "video",
-		}
-		ValidTReview := postRepo.TReview{
-			RatingStar: 3.5,
-			Comment:    "very good",
-			UID:        "1",
-		}
-		ValidTPackage := postRepo.TPackage{
-			Price: 450.0,
-		}
-		ValidTPost := postRepo.TPost{
-			ID:          ID,
-			Thumbnail:   ValidTThumbnail,
-			Name:        "tote bag handmade",
-			ReviewList:  []postRepo.TReview{ValidTReview},
-			PackageList: []postRepo.TPackage{ValidTPackage},
-			Detail:      "Tote Bag - Black",
-			Content:     "Featuring a boxy shape marked by clean lines and a minimalist design, this Perline bag in black is a timeless piece that will never go out of style. It also comes fitted with a magnetic closure for easy access to your belongings.",
-			CrafterID:   "456",
-		}
-		return ValidTPost, nil
-	}
 
-	return postRepo.TPost{}, nil
+// Jinny
+func UpdatePost(ID string, Post postRepo.TPost) error {
+	return nil
 }
+
+// Jinny
+func DeletePost(ID string) error {
+    post, err := GetPostById(ID)
+    if err != nil {
+        return err // Return error if GetPostById fails
+    }
+
+    // Check if the post exists
+    if post.ID != "" {
+        // Perform deletion operation in the database
+        // DeletePostFromDatabase(ID)
+        return nil // Return nil if deletion is successful
+    }
+
+    // Return error if the post does not exist
+    return errors.New("post not found")
+}
+
+func GetPostById(ID string) (postRepo.TPost, error) {
+    return postRepo.TPost{}, nil
+}
+
 
 // func GetPostById(ID string) (postRepo.TPost, error) {
 // wait for the connection to the database.
