94b0ea4b006076c1c9a608b0c6d7251a112f5700
punyaphatsura <punyaphat.sura@gmail.com>
2024-02-14 02:53:55 +0700

    style(Frontend): :art: lints

--- crafty-frontend/src/app/(routes)/(with-navbar)/_components/WaterfallContainer/_compoents/FeedCard/index.tsx
+++ crafty-frontend/src/app/(routes)/(with-navbar)/_components/WaterfallContainer/_compoents/FeedCard/index.tsx
@@ -1,14 +1,14 @@
-import Image from "next/image";
-import { FC } from "react";
+import Image from 'next/image';
+import { FC } from 'react';
 
 const FeedCard: FC = () => {
   return (
-    <div className="relative rounded-xl overflow-hidden">
+    <div className="relative overflow-hidden rounded-xl">
       <Image
         src={`https://picsum.photos/seed/${Math.random() * 1000}/500/${
           (Math.floor(Math.random() * 10) + 5) * 100
         }`}
-        className="w-full h-auto overflow-hidden"
+        className="h-auto w-full overflow-hidden"
         placeholder="blur"
         blurDataURL="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=="
         width={5000}
@@ -18,7 +18,7 @@ const FeedCard: FC = () => {
       />
 
       {/* Masking */}
-      <div className="absolute flex w-full h-full bg-[#000] z-20 top-0 opacity-0 hover:opacity-25 hover:cursor-pointer duration-100" />
+      <div className="absolute top-0 z-20 flex h-full w-full bg-[#000] opacity-0 duration-100 hover:cursor-pointer hover:opacity-25" />
     </div>
   );
 };
--- crafty-frontend/src/app/(routes)/(with-navbar)/layout.tsx
+++ crafty-frontend/src/app/(routes)/(with-navbar)/layout.tsx
@@ -1,11 +1,7 @@
-export default function NavbarLayout({
-  children
-}: {
-  children: React.ReactNode;
-}) {
+export default function NavbarLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col w-full min-h-screen">
-      <div className="flex flex-row w-full p-4 items-center h-16 bg-[#ffffff] sticky top-0 z-50">
+    <div className="flex min-h-screen w-full flex-col">
+      <div className="sticky top-0 z-50 flex h-16 w-full flex-row items-center bg-[#ffffff] p-4">
         <span className="text-xl">Navbar</span>
       </div>
       <div>{children}</div>
