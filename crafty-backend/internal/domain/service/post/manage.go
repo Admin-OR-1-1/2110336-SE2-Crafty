513bd96e116cf365165df53a62c43b7fd27eec82
JinnySarita <6432181821@student.chula.ac.th>
2024-02-13 12:14:08 +0700

    manage.go for post (service)

--- crafty-backend/internal/domain/service/post/manage.go
+++ crafty-backend/internal/domain/service/post/manage.go
@@ -1,7 +1,6 @@
 package post
 
 import (
-	"errors"
 	"fmt"
 
 	"github.com/Admin-OR-1-1/2110336-SE2-Crafty/crafty-backend/internal/domain/model"
@@ -42,7 +41,7 @@ func IsValidPost(Post model.TPost) bool {
 }
 
 func (s *PostService) CreatePost(Post model.TPost) error {
-	
+
 	if IsValidPost(Post) {
 		err := s.r.PostRepository.CreatePost(Post)
 		return err
@@ -52,27 +51,41 @@ func (s *PostService) CreatePost(Post model.TPost) error {
 }
 
 func (s *PostService) UpdatePost(Post model.TPost) error {
-	return nil
+	_, err := s.r.PostRepository.GetPostById(Post.ID)
+	if err != nil {
+		return err // Return error if GetPostById fails
+	}
+
+	err = s.r.PostRepository.UpdatePost(Post)
+	if err != nil {
+		return err // Return error if the update operation fails
+	}
+
+	return nil // Return nil if the update operation is successful
 }
 
-// Jinny
 func (s *PostService) DeletePost(ID string) error {
 	post, err := s.r.PostRepository.GetPostById(ID)
 	if err != nil {
 		return err // Return error if GetPostById fails
 	}
 
-	// Check if the post exists
 	if post.ID != "" {
 		// Perform deletion operation in the database
-		// DeletePostFromDatabase(ID)
+		err := s.r.PostRepository.DeletePost(ID)
+		if err != nil {
+			return err // Return error if deletion operation fails
+		}
 		return nil // Return nil if deletion is successful
 	}
 
-	// Return error if the post does not exist
-	return errors.New("post not found")
+	return fmt.Errorf("Post Not Found")
 }
 
 func (s *PostService) GetPostById(ID string) (model.TPost, error) {
-	return model.TPost{}, nil
+	post, err := s.r.PostRepository.GetPostById(ID)
+	if err != nil {
+		return model.TPost{}, err // Return error if GetPostById fails
+	}
+	return post, err
 }
