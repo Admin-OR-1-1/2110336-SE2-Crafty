b012e844debebab3bcdf0f67bbdf234c07850c06
JinnySarita <6432181821@student.chula.ac.th>
2024-02-13 11:36:33 +0700

    fix func name

--- crafty-backend/internal/domain/service/post/manage.go
+++ crafty-backend/internal/domain/service/post/manage.go
@@ -43,7 +43,7 @@ func IsValidPost(Post model.TPost) bool {
 
 func (s *PostService) CreatePost(Post model.TPost) error {
 	
-	if IsValidUser(Post) {
+	if IsValidPost(Post) {
 		err := s.r.PostRepository.CreatePost(Post)
 		return err
 	} else {
