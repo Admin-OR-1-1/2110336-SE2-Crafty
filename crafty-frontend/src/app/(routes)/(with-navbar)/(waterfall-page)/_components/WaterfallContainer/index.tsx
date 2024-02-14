9bcbf3132af0f5ffe107a9a4ab3a24b98ce9d4db
masternonnolnw <masternonnolnw@gmail.com>
2024-02-14 23:02:52 +0700

    feat: get liist

--- crafty-frontend/src/app/(routes)/(with-navbar)/(waterfall-page)/_components/WaterfallContainer/index.tsx
+++ crafty-frontend/src/app/(routes)/(with-navbar)/(waterfall-page)/_components/WaterfallContainer/index.tsx
@@ -1,11 +1,20 @@
 'use client';
 
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import FeedCard from './_components/FeedCard';
+import { apiClient } from '@/configs/axiosConfig';
 
 const WaterfallContainer: FC = () => {
+  const fetchPosts = async () => {
+    console.log('fetching posts');
+    const response = await apiClient.get('/post/list');
+    console.log('data', response.data);
+  };
   return (
     <div className="mx-auto grid w-full max-w-[1200px] grid-cols-3 gap-x-2 gap-y-2 p-4 lg:gap-x-4 lg:gap-y-4">
+      <button className="btn btn-primary" onClick={fetchPosts}>
+        test
+      </button>
       {Array.from({ length: 3 }).map((_, i) => {
         return (
           <div className="flex flex-col gap-4" key={i}>
