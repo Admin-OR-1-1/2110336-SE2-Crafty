115a44709a6908aebf8fdaa417c48656a0e8e120
Nontapan Sitthichotlertpakdee <masternonnolnw@gmail.com>
2024-03-12 20:41:30 +0700

    fix: secret not found

--- crafty-backend/src/modules/auth/common/jwt-const.ts
+++ crafty-backend/src/modules/auth/common/jwt-const.ts
@@ -1,7 +1,7 @@
 import { UserEntity } from 'src/modules/users/entities/user.entity'
 
 export const jwtConstants = {
-  secret: process.env.JWT_SECRET,
+  secret: process.env.JWT_SECRET || 'secret',
 }
 
 export type JwtPayload = {
